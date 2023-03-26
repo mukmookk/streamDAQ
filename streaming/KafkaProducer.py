fa3cec6b273a1b7027339411af734580cf2ed601
mukmookk <mukmookk@gmail.com>
2023-03-27 00:32:40 +0900

    TEST: Kafka connectivity test

---
+++ streaming/KafkaProducer.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from kafka import KafkaConsumer, KafkaProducer
+from time import sleep
+from json import dumps
+import json
+
+producer = KafkaProducer(bootstrap_servers=[':9092'],
+                         value_serializer=lambda x: dumps(x).encode('utf-8'))
+
+producer.send('nasdaq_prices', value="{'hello' : 'world'}")
