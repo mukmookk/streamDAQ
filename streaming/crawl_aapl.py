76842b14fdfdafbb7732d47f001bf0d177645343
mukmookk <mukmookk@gmail.com>
2023-03-26 22:21:03 +0900

    DEV: add basic logging configuration for crawl

--- streaming/crawl_aapl.py
+++ streaming/crawl_aapl.py
@@ -1,15 +1,26 @@
 from kafka import KafkaProducer
+import logging
 import requests
 import json
 
+# Set up logging to write to file
+logging.basicConfig(filename='../logs/crawl_aapl_price.log', format='%(asctime)s %(levelname)s %(message)s')
+
+# Set up ticker and URL
 symbol = "AAPL"
 url = f"https://api.nasdaq.com/api/quote/{symbol}/info?assetclass=stocks"
 
+# Generate kafka producer to generate 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 
+# Get data from API endpoint
 response = requests.get(url)
 data = response.json()
 
 price = data['data']['primaryData']['lastSalePrice']
-print(data)
+
+# Log the data
+log_msg = f"Symbol: {symbol}, Price: {price}"
+logging.info(log_msg)
+
 producer.send('nasdaq-prices', json.dumps({'symbol': symbol, 'price': price}).encode())
