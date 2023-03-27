bbacb4cde71243d5963cc66e77e191984cd0a9e6
mukmookk <mukmookk@gmail.com>
2023-03-27 14:54:10 +0900

    FIX: update reportLog function more abstracted

--- streaming/KafkaProducer.py
+++ streaming/KafkaProducer.py
@@ -42,13 +42,13 @@ def getDataFromRest(base_url, api_key, symbol):
     response_json = response.json()
 
     if 'Error Message' in response_json:
-        logging.fatal(f"Error fetching data from Alpha Vantage: {response_json['Error Message']}")
+        reportLog(f"Error fetching data from Alpha Vantage: {response_json['Error Message']}", "error")
         raise Exception("Error fetching data from Alpha Vantage:" + response_json['Error Message'])
     elif 'Note' in response_json:
-        logging.fatal(f"API call frequency limit reached: {response_json['Note']}")
+        reportLog(f"API call frequency limit reached: {response_json['Note']}", "error")
         raise Exception("API call frequency limit reached: " + response_json['Note'])
     elif 'Time Series (Daily)' not in response_json:
-        logging.fatal(f"Unexpected response from Alpha Vantage: {response_json}")
+        reportLog(f"Unexpected response from Alpha Vantage: {response_json}", "error")
         raise Exception("Unexpected response from Alpha Vantage: ", response_json)
     return response_json
 
@@ -58,10 +58,11 @@ if __name__ == '__main__':
     api_key = os.environ.get('APIKEY')
 
     if api_key is None:
+        reportLog("Environment variable 'APIKEY' not found", "error")
         raise ValueError("Environment variable 'APIKEY' not found")
     else:
-        print("API Key Successfully connected")
-        logging
+        reportLog("API Key Successfully connected", info)
+
     # Connect to Kafka Producer
     producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
                          value_serializer=lambda x: dumps(x).encode('utf-8'))
@@ -71,17 +72,15 @@ if __name__ == '__main__':
     # Block until the message is sent and get the metadata
     try:
         record_metadata = future.get(timeout=10)
-        print(f"Message sent successfully to {record_metadata.topic} "
+        reportLog(f"Message sent successfully to {record_metadata.topic} "
             f"partition {record_metadata.partition} "
-            f"offset {record_metadata.offset}")
+            f"offset {record_metadata.offset}", 'info')
         # Log the data
-        log_msg = f"Symbol: {symbol}, Price: {price}"
-        logging.info(log_msg)
+        reportLog(f"Symbol: {symbol}, Price: {0}")
     except Exception as e:
         error_msg = f"Error sending message: {e}"
-        print(error_msg)
-        logging.warn(error_msg)
+        reportLog(error_msg, 'error')
     finally:
         # Close the producer to flush any remaining messages
         producer.close()
-        logging.info("close kafka producer process")
+        reportLog("close kafka producer process", 'info')
