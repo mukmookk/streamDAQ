812865d9f826492fa2cfcdee21d7b62f449a774c
mukmookk <mukmookk@gmail.com>
2023-03-27 13:03:50 +0900

    FIX: update code more straightforward

--- streaming/KafkaConsumer.py
+++ streaming/KafkaConsumer.py
@@ -1,17 +1,14 @@
-import pandas as pd
+mport pandas as pd
 from kafka import KafkaConsumer
-from time import sleep
-from json import dumps, loads
-import json
 
 consumer = KafkaConsumer(
-        'nasdaq_prices',
-        bootstrap_servers=['127.0.0.1:9092'],
-        value_deserializer=lambda x : loads(x.decode('utf-8')))
+'nasdaq_prices',
+bootstrap_servers=['127.0.0.1:9092'],
+value_deserializer=lambda x : json.loads(x.decode('utf-8')))
 
-print("Message Starts")
+print("Messages start streaming...")
 
-for c in consumer:
-    print(c, c.value)
+for message in consumer:
+    print(f"Received message: {message.value}")
 
-print("Message Ends")
+print("Message streaming finished.")
