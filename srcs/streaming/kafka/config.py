ae794737c9a6200d204f44e44b12830494b31fd3
mukmookk <mukmookk@gmail.com>
2023-04-08 22:34:51 +0900

    Test: config.py OK

--- srcs/streaming/kafka/config.py
+++ srcs/streaming/kafka/config.py
@@ -6,6 +6,14 @@ import datetime
 WD = os.environ.get('DAQWD') if os.environ.get('DAQWD') else os.getcwd()
 timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 filename = f"{WD}/logs/crawl_{timestamp}.log"
+log_dir = os.path.join(WD, 'logs')
+os.makedirs(log_dir, exist_ok=True)
 
-logging.basicConfig(filename=filename, format='%(asctime)s %(levelname)s %(message)s')
-logging.getLogger().setLevel(logging.INFO)
+try:
+    logging.basicConfig(filename=filename, format='%(asctime)s %(levelname)s %(message)s')
+    logging.getLogger().setLevel(logging.INFO)
+except:
+    log_dir = os.path.join(WD, 'logs')
+    os.makedirs(log_dir, exist_ok=True)
+    logging.basicConfig(filename=filename, format='%(asctime)s %(levelname)s %(message)s')
+    logging.getLogger().setLevel(logging.INFO)
