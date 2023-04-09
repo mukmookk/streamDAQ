d1aede54d135175a496585fe46c4713a9987332b
mukmookk <mukmookk@gmail.com>
2023-04-09 19:39:47 +0900

    #5 Dev: Add additional condition

--- srcs/path.py
+++ srcs/path.py
@@ -2,4 +2,9 @@ import sys
 import os
 
 root_dir = os.path.abspath(os.path.dirname(__file__))
-sys.path.insert(0, root_dir)
+
+if root_dir not in sys.path:
+    sys.path.insert(0, root_dir)
+    print(f"Added {root_dir} to Python path")
+
+print(f"Current Python path list: {sys.path}")
