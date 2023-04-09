36847def67cb391692f5e60ca68d47153f1b378c
mukmookk <mukmookk@gmail.com>
2023-04-09 19:50:35 +0900

    #5 Fix: stdout message

--- srcs/path.py
+++ srcs/path.py
@@ -6,5 +6,5 @@ root_dir = os.path.abspath(os.path.dirname(__file__))
 if root_dir not in sys.path:
     sys.path.insert(0, root_dir)
     print("Added {} to Python path".format(root_dir))
-
-print("Already in Python path: {}".format(root_dir))
+else:
+    print("Already in Python path: {}".format(root_dir))
