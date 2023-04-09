8cdffa7a99ae0a7628a57d8818dcd295b7ee735f
mukmookk <mukmookk@gmail.com>
2023-04-09 19:41:11 +0900

    #5 Fix: Syntax error in previous commit

--- srcs/path.py
+++ srcs/path.py
@@ -5,6 +5,6 @@ root_dir = os.path.abspath(os.path.dirname(__file__))
 
 if root_dir not in sys.path:
     sys.path.insert(0, root_dir)
-    print(f"Added {root_dir} to Python path")
+    print("Added {} to Python path".format(root_dir))
 
-print(f"Current Python path list: {sys.path}")
+print("Current Python path list: {}".format(sys.path))
