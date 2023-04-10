8209a177c851fad8f760b2c086324820fda79b83
mukmookk <mukmookk@gmail.com>
2023-04-10 19:31:23 +0900

    Dev: get market status

--- srcs/streaming/messageQueue/utils/api.py
+++ srcs/streaming/messageQueue/utils/api.py
@@ -43,3 +43,30 @@ def searchSymbol(_keywords, _apiKey):
     else:
         utils.reportLog("No matching tickers found.", "info")
 
+def marketStatus(apikey):
+    url = "https://www.alphavantage.co/query"
+    function = "MARKET_STATUS"
+    params = {
+        "function": function,
+        "apikey": apikey
+    }
+    try:
+        response = requests.get(url, params=params)
+    except requests.exceptions.RequestException as e:
+        utils.reportlog(f"Error fetching data from Alpha Vantage: {e}", "error")
+        raise SystemExit(e)
+
+    try:
+        data = json.loads(response.text)
+    except json.decoder.JSONDecodeError as e:
+        utils.reportlog(f"Error decoding JSON: {e}", "error")
+        raise SystemExit(e)
+
+    if "Global Quote" in data:
+        status = data["Global Quote"]["09:30:00"]
+        if status == "0.0000":
+            utils.reportlog("Market is closed.", "info")
+        else:
+            utils.reportlog("Market is open.", "info")
+    else:
+        utils.reportlog("Error fetching market status.", "error")
