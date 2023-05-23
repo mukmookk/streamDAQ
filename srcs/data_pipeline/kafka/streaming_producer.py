5d4872ba60b0433fdc6c96970e8b8c05178489dc
MOOK <mukmookk@gmail.com>
2023-05-23 16:19:52 +0900

    Pytest: post msg thorugh multiple test topics

--- srcs/data_pipeline/kafka/streaming_producer.py
+++ srcs/data_pipeline/kafka/streaming_producer.py
@@ -1,13 +1,40 @@
-import pytest
-from data_pipeline.kafka.streaming_producer import KafkaProducer
+from data_pipeline.extraction.extractor import StreamingDataExtractor
 import json
+from confluent_kafka import Producer
 
-def test_kafka_producer():
-    # Instantiate the producer with the broker's hostname and port
-    kafka_producer = KafkaProducer('kafka1', 9092)
-    topic = 'test-topic'
-    data = {'test': 'kafka-communication-test'}
-    kafka_producer.produce_msg_from_function(data, topic, dumped=False)
-    kafka_producer.flush()
+class KafkaProducer():
+    def __init__(self, hostname, port):
+        self.hostnaem = hostname
+        self.port = port
+        self.producer = Producer({'bootstrap.servers': f'{self.hostnaem}:{self.port}'})
 
-test_kafka_producer()
+    def delivery_report(self, err, msg):
+        if err is not None:
+            print('Message delivery failed: {}'.format(err))
+        else:
+            print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+    def produce(self, topic, data):
+        self.producer.produce(topic, data.encode('utf-8'), callback=self.delivery_report)
+        self.producer.poll(0)
+        
+    def produce_msg_from_file(self, file_path, topic):
+        with open(file_path, encoding='utf-8') as json_file:
+            data = json.load(json_file)
+            for item in data:
+                message = json.dumps(item)
+                self.produce(topic, json.dumps(item))
+            self.producer.flush()
+            
+    def produce_msg_from_function(self, data, topic, dumped=False):
+        if not dumped:
+            data = json.dumps(data)
+        for item in data:
+            self.produce(topic, item)
+        self.producer.flush()
+        
+    def flush(self):
+        self.producer.flush()
+
+if __name__ == "__main__":
+    pass
